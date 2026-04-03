d085d75272017f4fc4f859c16e2b66005e4a069b
Stachu Korick <hello@stachu.net>
2026-04-03 12:17:25 -0400

    Add x86-64 encoding unit tests (11 tests, all passing)
    Tests verify correct machine code bytes for:
    - MOV reg,reg (including R8-R15 REX handling)
    - MOV reg,imm32
    - ADD/SUB with imm8
    - PUSH/POP (including extended registers)
    - RET, SYSCALL, CQO
    - XOR zeroing, NEG, ADD reg, CMP imm, IMUL, SHL

--- src/Tests/test-suite-tooling/TestRunner.fs
+++ src/Tests/test-suite-tooling/TestRunner.fs
@@ -225,6 +225,7 @@ let main args =
         { Name = "Pass Test Runner Tests"; Tests = PassTestRunnerTests.tests }
         { Name = "Progress Bar Tests"; Tests = ProgressBarTests.tests }
         { Name = "Encoding Tests"; Tests = EncodingTests.tests }
+        { Name = "x86-64 Encoding Tests"; Tests = X86_64EncodingTests.tests }
         { Name = "Binary Tests"; Tests = BinaryTests.tests }
         { Name = "Type Checking Tests"; Tests = TypeCheckingTests.tests }
         { Name = "Parallel Move Tests"; Tests = ParallelMoveTests.tests }
